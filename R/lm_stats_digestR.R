2a6d64386f434877c603980d33f81cf0d2cb4b7e
Drew Steen <andrew.decker.steen@gmail.com>
2015-01-02 21:20:38 -0500

    removed unnecessary comments

--- R/lm_stats_digestR.R
+++ R/lm_stats_digestR.R
@@ -1,30 +1,20 @@
 ##' Returns statistics for linear regressions (e.g. slope, intercept, error, etc)
-##' 
+##'
 ##' @param d Data frame containing at least two numeric columns
 ##' @param xvar Numeric column to be used as the independant variable for the regression
 ##' @param yvar Numeric column to be used as the dependant variable for the regression
 ##' @details include some details here
 ##' @return Returns a one-row data frame containing (at present): slope, intercept, slope standard error, intercept standard error, p value, r-squared, and number of points.
 ##' @export
-##' require(ggplot2)
-##' lm_stats(mpg, xvar="cty", yvar="hwy")
 
+lm_stats <- function(d, xvar, yvar, alpha=0.05) {
 
-lm_stats <- function(d, xvar, yvar) {
-  # Function to safely return the slope, intercept, slope.se, int.se, rsq, and pvalue of a linear model
-  #print(d[1, ])
-  
-  ##### CHeck that this tryCatch syntax is correct
-  #get_model <- function(m) {
-  # m <- tryCatch(
-  #    model <- lm(d[ , yvar] ~ d[ , xvar]),
-  #    error <- NA,
-  #    finally={})
-  #}
+  # Create model
   m <- lm(d[ , yvar] ~ d[ , xvar]) # Should wrap this in a tryCatch too!
-  # Alsom improve error message if xvar or yvar arent in d
+
+  # Also improve error message if xvar or yvar arent in d
   sum_m <- summary(m)
-  
+
   # Function to safely get the slope
   get_slope <- function(m) {
     slope <- tryCatch(
@@ -35,7 +25,7 @@ lm_stats <- function(d, xvar, yvar) {
     )
     return(slope)
   }
-  
+
   # Function to safely get the intercept
   get_int <- function(m) {
     int <- tryCatch(
@@ -46,7 +36,7 @@ lm_stats <- function(d, xvar, yvar) {
     )
     return(int)
   }
-  
+
   # Function to safely get the standard error of the intercept
   get_int.se <- function(m) {
     int.se <- tryCatch(
@@ -57,7 +47,7 @@ lm_stats <- function(d, xvar, yvar) {
     )
     return(int.se)
   }
-  
+
   # Function to safely get the standard error of the slope
   get_slope.se <- function(m) {
     slope.se <- tryCatch(
@@ -68,7 +58,7 @@ lm_stats <- function(d, xvar, yvar) {
     )
     return(slope.se)
   }
-  
+
   # Function to safely get the p value
   get_p_val <- function(m) {
     pval <- tryCatch(
@@ -80,10 +70,10 @@ lm_stats <- function(d, xvar, yvar) {
         return(pval)
       },
       finally={}
-    )    
+    )
     return(pval)
   }
-  
+
   # Function to safely get the r-squared value
   get_rsq <- function(m) {
     rsq <- tryCatch(
@@ -94,14 +84,15 @@ lm_stats <- function(d, xvar, yvar) {
     )
     return(rsq)
   }
-  
-  n <- nrow(d[!is.na(d[ , xvar]) & !is.na(d[ , yvar]), ]) # I can't really think of how this would throw errors
-  
+
+  # Determine number of points
+  n <- nrow(d[!is.na(d[ , xvar]) & !is.na(d[ , yvar]), ])
+
   # Return the parameters in a 1-row data frame (the most convenient format for plyr functions)
-  data.frame(slope = get_slope(m), 
-             int = get_int(m), 
-             slope.se=get_slope.se(m), 
-             int.se=get_int.se(m), 
+  data.frame(slope = get_slope(m),
+             int = get_int(m),
+             slope.se=get_slope.se(m),
+             int.se=get_int.se(m),
              pval=get_p_val(m),
              rsq = get_rsq(m),
              n=n)
