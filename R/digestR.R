0b018ea98303395306cffafb6df2ee0e0ee77936
Drew Steen <andrew.decker.steen@gmail.com>
2015-01-02 19:31:56 -0500

    semi-complete package and data documentation

---
+++ R/digestR.R
@@ -0,0 +1,39 @@
+##' digestR
+##'
+##' @name digestR
+##' @docType package
+##' @import plyr reshape2 ggplot2
+NULL
+
+##' An example dataset to be used with digestR
+##'
+##' This data frame is some stuff
+##'
+##' @docType data
+##' @keywords datasets
+##' @name ex_metagenome_input
+##' @usage data(ex_metagenome_input)
+##' @format A wide-format data frame with some columns and some rows
+NULL
+
+##' An example sample key
+##'
+##'
+NULL
+##'
+##' A draft "function map"
+##'
+##' The function map pairs pfam units with their proposed function
+##'
+##' \itemize{
+##'   \item sample.name
+##'   \item sum.reads
+##'   \item lat
+##'   \item lon
+##'   \item depth
+##'   \item time
+##' }
+##' @docType data
+##' @keywords datasets
+##' @name ex_sample_key
+##' @usage data(ex_sample_key)
