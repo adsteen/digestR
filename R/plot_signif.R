e95fcb271110790ce66a01de313f6b6283c080c4
Drew Steen <andrew.decker.steen@gmail.com>
2015-01-04 16:40:19 -0500

    Changed plot format for depth plots

--- R/plot_signif.R
+++ R/plot_signif.R
@@ -24,16 +24,26 @@ plot_signif <- function(d, indep.var="depth", dep.var="frac.reads", variable="pf
     stop(paste("There is no column in d named", dep.var))
   }
 
+  # Identify pfams with significant changes
   sig <- find_signif(d, indep.var=indep.var, dep.var=dep.var, alpha=alpha)
 
   # Make plot
   p <- ggplot(sig, aes_string(x=indep.var, y=dep.var)) +
     geom_point() +
-    geom_smooth(method="lm", se=FALSE) +
-    scale_x_reverse() +
-    expand_limits(y=0) +
-    coord_flip() +
-    facet_wrap(as.formula(paste("~", variable)))
+    geom_smooth(method="lm", se=FALSE) #+
+    #scale_x_reverse() +
+    #expand_limits(y=0) +
+    #coord_flip() +
+
+
+  if(indep.var=="depth") {
+    p <- p + scale_x_reverse() +
+      expand_limits(y=0) +
+      coord_flip() +
+      facet_wrap(as.formula(paste("~", variable)))
+  } else {
+    p <- p + facet_wrap(as.formula(paste("~", variable)), scales="free")
+  }
 
   p
 }
